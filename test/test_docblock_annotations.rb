edd213425a6faaf9827f129a8924649776314a21
kematzy <kematzy@gmail.com>
2011-08-27 10:35:31 +0800

    Amended tests to reflect changes
    TODO: need to test more versions

--- test/test_docblock_annotations.rb
+++ test/test_docblock_annotations.rb
@@ -5,9 +5,9 @@ class RoccoDocblockAnnotationsTest < Test::Unit::TestCase
     r = Rocco.new( 'test', '', { :language => "c", :docblocks => true } ) { "" } # Generate throwaway instance so I can test `parse`
     assert_equal(
       [
-        "Comment\n\n> **param** mixed foo  \n> **return** void  "
+        "Comment\n\n+  **param** (mixed) - foo  \n+  **param** (string) - bar  \n+  **return** (void)   "
       ],
-      r.docblock( ["Comment\n\n@param mixed foo\n@return void"] )
+      r.docblock( ["Comment\n\n@param mixed foo\n@param string bar\n@return void"] )
     )
   end
   def test_highlighted_in_blocks
@@ -15,7 +15,7 @@ class RoccoDocblockAnnotationsTest < Test::Unit::TestCase
     highlighted = r.highlight( r.split( r.parse( "/**\n * Comment\n * @param type name\n */\ndef codeblock\nend\n" ) ) )
 
     assert_equal(
-      "<p>Comment</p>\n\n<blockquote><p><strong>param</strong> type name</p></blockquote>\n",
+      "<p>Comment\n+  <strong>param</strong> (type) &ndash; name</p>\n",
       highlighted[0][0]
     )
   end
